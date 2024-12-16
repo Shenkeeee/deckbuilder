aca483e1632c35ccd7d686e2b245a419fc21515c
Shenkee-Fred <subicz.mate@frederik-technologies.com>
2024-12-16 23:34:42 +0100

    removed test files, added cache and .env to gitignore

--- src/main-container/carddata-container/card-instance.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CardInstance } from './card-instance';
-
-describe('CardInstance', () => {
-  it('should create an instance', () => {
-    expect(new CardInstance()).toBeTruthy();
-  });
-});
