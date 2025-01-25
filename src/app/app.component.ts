84dde793e07b937f49e41011d269fdfa69bd8897
Shenkeeee <mate.subicz@gmail.com>
2025-01-25 13:38:06 +0100

    redirecting from firebase default url to the new deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarContainerComponent } from "../navbar-container/navbar-container.component";
-
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarContainerComponent, ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'deckbuilder';
+export class AppComponent implements OnInit {
+  title = 'DoP Deckbuilder';
+
+  ngOnInit() {
+    this.redirectFromDefault();
+  }
+
+  redirectFromDefault() {
+    if (
+      window.location.hostname == environment.defaultUrl ||
+      window.location.hostname == environment.secondaryDefaultUrl
+    ) {
+      window.location.href = environment.mainUrl;
+    }
+  }
 }
